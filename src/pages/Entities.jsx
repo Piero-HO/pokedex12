e2c49a0f2ad8f306a60a71f2918687c5f0f193dc
Piero-HO <piero.huaytalla@tecsup.edu.pe>
2026-06-06 21:08:51 -0500

    Filtro de busqueda Pokemon

--- src/pages/Entities.jsx
+++ src/pages/Entities.jsx
@@ -3,6 +3,7 @@ import "../App.css";
 
 function Entities() {
   const [pokemons, setPokemons] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const loadPokemons = async () => {
@@ -25,15 +26,27 @@ function Entities() {
     loadPokemons();
   }, []);
 
+  const filteredPokemons = pokemons.filter((pokemon) =>
+  pokemon.name.toLowerCase().includes(search.toLowerCase())
+);
+
   return (
     <div className="container">
       <div className="hero">
-        <h1>Pokédex Explorer</h1>
-<p>Información detallada de los Pokémon obtenidos desde la PokéAPI</p>
-      </div>
+  <h1>Pokédex Explorer</h1>
+  <p>Información detallada de los Pokémon obtenidos desde la PokéAPI</p>
+
+  <input
+    type="text"
+    placeholder="Buscar Pokémon..."
+    value={search}
+    onChange={(e) => setSearch(e.target.value)}
+    className="search-input"
+  />
+</div>
 
       <div className="grid">
-        {pokemons.map((pokemon) => (
+        {filteredPokemons.map((pokemon) => (
           <div
   className="card"
   key={pokemon.id}
